79c2aa63af003df2567649fb4e5eaa449a7a72a4
Dan <tyukavindenis@gmail.com>
2020-09-28 21:22:34 +0300

    desktop create style

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,24 @@
 
 (function (w, d) {
 
-d.addEventListener('load', () => {
+w.addEventListener('load', function () {
 
 	console.log('Load!');
+
+	new Siema({
+		selector: '.siema',
+		duration: 200,
+		easing: 'ease-out',
+		perPage: 2,
+		startIndex: 0,
+		draggable: true,
+		multipleDrag: true,
+		threshold: 20,
+		loop: false,
+		rtl: false,
+		onInit: () => {},
+		onChange: () => {},
+	});
 
 });
 
