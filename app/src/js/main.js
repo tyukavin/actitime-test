0fc17d1bdde77f6bafe2228092fa8325204d38d3
Dan <tyukavindenis@gmail.com>
2020-09-29 12:42:54 +0300

    add style transition

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -4,9 +4,9 @@
 
 w.addEventListener('load', function () {
 
-	console.log('Load!');
-
-	new Siema({
+	var $paginationBTNS;
+	
+	let slider = new Siema({
 		selector: '.siema',
 		duration: 200,
 		easing: 'ease-out',
@@ -17,9 +17,48 @@ w.addEventListener('load', function () {
 		threshold: 20,
 		loop: false,
 		rtl: false,
-		onInit: () => {},
-		onChange: () => {},
+		onInit: () => {
+			printSlideIndex.call(this);
+		},
+		onChange: () => {
+			printSlideIndex.call(this);
+		},
 	});
+
+
+	Siema.prototype.addPagination = function() {
+		const dots = d.createElement('div');
+        dots.classList.add('dots');
+        this.selector.appendChild(dots);
+        for (let i = 0; i < this.innerElements.length; i++) {
+          const btn = d.createElement('div');
+          btn.classList.add('dot');
+          btn.addEventListener('click', () => this.goTo(i));
+          dots.appendChild(btn);
+        }
+        $paginationBTNS = $siemaPagination.querySelectorAll('.dot');
+	}
+
+	function printSlideIndex() {
+        const SIEMA_CURRENT = this.currentSlide;
+
+        if ($paginationBTNS) {
+          let paginationBTNS = Array.prototype.slice.call($paginationBTNS)
+          paginationBTNS
+            .forEach(btn => {
+              btn.classList.remove('is-active');
+            });
+          paginationBTNS[SIEMA_CURRENT].classList.add('is-active');
+        }
+        
+        this.innerElements.forEach((slide, i) => {
+          const addOrRemove = i === this.currentSlide ? 'add' : 'remove';
+          this.innerElements[i].classList[addOrRemove]('show');
+        });
+      }
+
+	slider.addPagination();
+
 
 });
 
