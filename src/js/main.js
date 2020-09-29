2f2996da976b37e68f547a2d977a0ae68189a2ba
Dan <tyukavindenis@gmail.com>
2020-09-29 21:14:12 +0300

    mobile version

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,26 @@ w.addEventListener('load', function () {
 			clickable: true,
 			el: '.swiper-pagination',
 		},
+		breakpoints: {
+		    // when window width is >= 320px
+		    0: {
+		      slidesPerView: 1,
+		      slidesPerGroup: 1,
+		      spaceBetween: 20
+		    },
+		    // when window width is >= 480px
+		    750: {
+		      slidesPerView: 3,
+		      spaceBetween: 30
+		    },
+		    // when window width is >= 640px
+		    992: {
+		      slidesPerView: 4,
+		      spaceBetween: 40
+		    }
+		  }
 	});
+
 
 });
 
