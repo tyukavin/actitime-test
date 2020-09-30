7757d1a23d4ba5996efce6f59ae2bf86792e3697
Dan <tyukavindenis@gmail.com>
2020-09-30 19:13:47 +0300

    fix bag & add favicon

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,9 @@ w.addEventListener('load', function () {
 	var btnMenu = d.querySelector('.menu-btn'),
 		mobileMenu = d.querySelector('.header__left'),
 		mainTitle = d.querySelector('.main__title'),
-		mainDesc = d.querySelector('.main__desc');
+		mainDesc = d.querySelector('.main__desc'),
+		btnMenuInside = d.querySelector('.icon-arrow-mobile'),
+		menuInside = d.querySelector('.menu-inside');
 
 	mainTitle.classList.add('is-show');
 	mainDesc.classList.add('is-show');
@@ -15,6 +17,13 @@ w.addEventListener('load', function () {
 	btnMenu.addEventListener('click', function() {
 		this.classList.toggle('is-active');
 		mobileMenu.classList.toggle('is-show');
+		btnMenuInside.classList.remove('is-active');
+		menuInside.classList.remove('is-show');
+	});
+
+	btnMenuInside.addEventListener('click', function() {
+		this.classList.toggle('is-active');
+		menuInside.classList.toggle('is-show');
 	});
 	
 	var mySwiper = new Swiper('.swiper-container', {
@@ -33,13 +42,14 @@ w.addEventListener('load', function () {
 		      	slidesPerGroup: 1,
 		      	spaceBetween: 20
 		    },
-		    // when window width is >= 750px
-		    750: {
-		      	slidesPerView: 3,
-		      	spaceBetween: 30
+		    // when window width is >= 0px
+		    992: {
+		    	slidesPerView: 1,
+		      	slidesPerGroup: 1,
+		      	spaceBetween: 0
 		    },
 		    // when window width is >= 992px
-		    992: {
+		    1200: {
 		      	slidesPerView: 2,
 		      	spaceBetween: 150,
 				slidesPerGroup: 3,
